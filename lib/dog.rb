1ca91120c953107ab08a84afc08e4e83669f9b5c
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-09-21 20:33:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 # Your code goes here!
 class Dog
-  def initialize(.name)
-    @name = .name
-  end
-
-  def .name=(name)
+  def initialize(name)
     @name = name
   end
 
-  def .name
+  def name=(name)
+    @name = name
+  end
+
+  def name
     @name
   end
 end
